3e0b6d6233e633a808e46e92b5b2b53946e21cd6
Sarah Osgood <sarahjs117@gmail.com>
2021-02-06 16:10:51 -0700

    Consolidate notes in Profile with a list of files that need to be modified added or referenced and include annotations on lines for code needed in Profile

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -1,82 +1,35 @@
 //TODO: for profile updates
 /*
+This will need to add button for Orders and to Edit Profile
+  - for Edit Profile button reference: web/src/modules/admin/product/List.js             line 30, 114
 
-This will need to be updated to add:
-1. edit profile button (to display Modal)
-2. shipping address display
-3. image display
-4. description
+Will also need to add image, description, shipping address to render
 
-
-will need to functionality to POST/GET to/from BE:
-updated email
-shipping address
-description
-image **This may need to be seperated from other profile state
-We found how an image is uploaded by admin for a new product. 
-We believe we can use that code for uploading an image for a user!
-see TODO in:
- - src/modules/admin/product/CreateOrEdit.js
- - src/modules/admin/common/api/actions.js
-
-BackEnd files associated with editing profile:
- - api/src/modules/user/model.js
- - api/src/modules/user/mutation.js
- - api/src/modules/user/query.js
- - api/src/modules/user/resolvers.js
- - api/src/modules/user/types.js
-
-will need functionality on edit button to display Profile Edit Modal
-**OR we can use the functionality of +ADD from the admin/product/CreateOrEdit.js 
-to route to an edit for profile.
-
-New Files/Components needed for this:
-EditProfile (modal) probably a Class Component to update State
-**See below export for notes on what will be needed in this component
-*/
-
-//TODO: for order history/next delivery date available update by user
-/*
-1. button to see product history/next shipment (in src/modules/common/header/Header.js) which will route to new page for user
-- Will need functionality to route to product history/shipment
-- Will need a class component for ProductHistory (or OrderHistory?)
-  **See more on this below export!
-
-This component will render the order history page with:
-1. next delivery expected with:
-  - data about products in the crate (we think) (api GET)
-  - expected delivery date (api GET)
-  - field/button to update the users available date for crate delivery (api POST adn then GET)
-2. All previous products with:
-  - data about each product (api GET)
-  - boolean for whether that specific item was returned by the user (api GET/POST/GET)
-   **We may not make a POST/GET for this. as we may not want to add the functionality for this MVP
-
-New Files/Components needed for this:
-ProductHistory Class Component
-**See below export for notes on what will be needed in this component
+Add Orders.js to user directory
+Add ProfileForm.js to user directory
+See notes on each of those files!
 */
 
 /*
-NOTES from team discussions:
-I think that we will likely use a lot of the logic from src/module/crate directory 
+List of files that need to be modified/added for FE:
+- web/src/modulues/user/Profile.js          line 62, 65, 66, 70
+- web/src/modulues/user/Orders.js           NEW
+- web/src/modulues/user/ProfileForm.js      NEW
+- web/src/modulues/user/actions.js          line 86
+- web/src/modulues/user/state.js            line 23
+- web/src/modulues/common/header/Header.js  line 58
 
-I am noticing there is also a src/module/product. It seems we could use pieces of that for order history?
+??web/src/modulues/user/Login.js ??
 
-on page load:
-  - store will have been updated with user profile data on login
-  - pull from store - photo, email, shipping address, description
+List of files to reference for adding the new features:
+add image:
+- web/src/modules/admin/product/CreateOrEdit.js     line 173, 310, 319
+- web/src/modules/admin/product/List.js             line 30, 114
+- web/src/modules/common/api/actions.js             line 17
 
-edit profile button opens Form Modal:
-  - has it's own state that is pulled from store on pageload
-  - email, shipping, description
-  - handleChange for fields
-  - handleSubmit
-    - get dispatch from user/api/action.js
-
-update profile image:
-  - 
 */
+
+
 
 // Imports
 import React from 'react'
@@ -143,56 +96,3 @@ function profileState(state) {
 }
 
 export default connect(profileState, { logout })(Profile)
-
-
-/* TODO:
-New File:
-src/modules/user/EditProfile.js
-Class Component
-    this.state = {
-      user: {
-        email: '',
-        password: '',
-        description: '',
-        shippingAddress: '',
-        profileImage: '', // probably need a default image here?
-        userID: ???
-      }
-    }
-
-    Will need defaults in the render for empty strings and for the default image if
-    user has not uploaded an image yet.
-
-    **See for further logic details on this section:
-      - src/modules/admin/product/CreateOrEdit.js
-      - src/modules/admin/common/api/actions.js
-    onChange method to handle user inputs in fields
-    onUpload method to handle user image input
-    import { renderIf } from '../../../setup/helpers' to render the image a user uploads (before submitting)
-    onSubmit method to handle submission of changes
-
-*/
-
-/* TODO:
-New File:
-src/modules/user/ProductHistory.js
-Class Component
-
-componentDidMount to call GET of next delivery crate AND Products already delieverd
-And .then setState with that information
-
-render: 
-  Next Delivery
-    I think we should be able to pull from:
-    src/modules/admin/crate/Item.js for the logic of displaying this. 
-    It will obviously need modifications for specifics of what we want displayed
-
-  ProductHistory
-    We should be able to pull from:
-    src/admin/product/List.js for the logic of displaying order history
-    
-NOTES:
- - maybe could add an api call on Login to get crates/product history - We decided NO
-
- - To get all the past and future product ordering data is probably 1 call!
-*/
