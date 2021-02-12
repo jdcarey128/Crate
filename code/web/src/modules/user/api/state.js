73767b8373595d9928811deff3c45ec22f44cf6d
Sarah Osgood <sarahjs117@gmail.com>
2021-02-12 12:58:40 -0700

    Complete action to update store but there is an errror in this commit in ProfileForm line 127 with the response.data.errors that needs to be worked on

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -1,6 +1,6 @@
 // App Imports
 import { isEmpty } from '../../../setup/helpers'
-import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
+import { SET_USER, UPDATE_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
 // Initial State
 export const userInitialState = {
@@ -19,6 +19,17 @@ export default (state = userInitialState, action) => {
         isAuthenticated: !isEmpty(action.user),
         details: action.user,
       }
+
+    case UPDATE_USER:
+      return {
+        ...state,
+        details: action.user,
+      }
+
+    // case UPDATE_USER_RESPONSE:
+    //   return {
+
+    //   }
 
     case LOGIN_REQUEST:
       return {
